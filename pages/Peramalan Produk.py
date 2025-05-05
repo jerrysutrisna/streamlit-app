ebb5884eadfdc9bf4cbb9d3b80962bda760a3d6f
jerrysutrisna <99080118+jerrysutrisna@users.noreply.github.com>
2025-05-05 22:46:24 +0900

    Update Peramalan Produk.py

--- pages/Peramalan Produk.py
+++ pages/Peramalan Produk.py
@@ -8,7 +8,7 @@ from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
 # Judul
-st.title("Peramalan Permintaan Produk dengan SARIMA")
+st.title("Analisis dan Visualisasi Peramalan Permintaan Produk")
 
 # File uploader
 uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx"])
